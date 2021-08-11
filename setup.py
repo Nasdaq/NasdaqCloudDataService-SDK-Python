3512270815fd9bcdf2fdf70d3f1f401b5c4ba645
Spencer Sortman <spesor@us22macg4ahjgh6.org.nasdaqomx.com>
2021-08-11 15:17:16 -0400

    Create python packaging files

---
+++ setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get long description from README
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+    name='ncdssdk',
+    version='0.0.14.dev1',
+    description='A Python SDK for developing applications to access the NCDS API',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Nasdaq',
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 1 - Planning',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: Apache Software License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate you support Python 3. These classifiers are *not*
+        # checked by 'pip install'. See instead 'python_requires' below.
+        'Programming Language :: Python :: 3.9',
+    ],
+    keywords='Nasdaq, NCDS, ncdssdk',
+    packages=find_packages(),
+    python_requires='>=3.9, <4',
+    install_requires=open("requirements.in").readlines(),
+
+    include_package_data=True,
+    # We could possibly use entry_points parameter with console_scripts here to specify the NCDSSession script
+
+    project_urls={  # Optional
+        # FIXME: github location
+        'Source': 'https://github.com',
+    },
+)
